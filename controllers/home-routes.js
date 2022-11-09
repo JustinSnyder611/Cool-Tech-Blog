c2485469bcde6eeef9fc0af862164f20fc360f11
JustinSnyder611 <JustinSnyder611@gmail.com>
2022-11-09 13:42:51 -0600

    Fixed the routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const { User, Message } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
+    const messageData = await User.findAll({
+      include: [
+        {
+          model: Message,
+          attributes: ['message', 'user_username'],
+        },
+      ],
+    });
+
+    const messages = messageData.map((message) =>
+      message.get({ plain: true })
+    );
     res.render('homepage', {
+      messages,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
@@ -10,6 +25,15 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/dashboard', withAuth, (req, res) => {
+  try {
+    res.render('dashboard');
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
 
 // Login route
 router.get('/login', (req, res) => {
